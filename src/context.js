9d73cd77e51c155d1d4dc612c47e6777e9b01621
Pawan Benjamin <pawanbenjamin@gmail.com>
2021-07-10 13:00:00 -0400

    Context Demo Added

---
+++ src/context.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const MyContext = React.createContext()
+
+export const ThingsProvider = MyContext.Provider
+
+export default MyContext
+
--- src/index.js
+++ src/index.js
@@ -1,26 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import ReactDOM from "react-dom";
 
+import MyContext, { ThingsProvider } from "./context";
+
+
 const Clicker = ({ inc }) => {
+  const things = useContext(MyContext)
+  console.log(things)
   return <button onClick={inc}>Click Me</button>;
 };
 
 const Count = ({ count }) => {
+  const things = useContext(MyContext)
+  console.log(things)
   return <h1>The Count is {count} </h1>;
 };
 
 const App = () => {
   let [count, setCount] = useState(0);
 
+  let stuff = [
+    {
+      name: 'Pawan'
+    }, { name: 'Benjamin'}
+  ]
+
   const inc = () => {
     setCount(count + 1);
   };
 
   return (
-    <div>
-      <Clicker inc={inc} />
-      <Count count={count} />
-    </div>
+    <ThingsProvider value={stuff}>
+      <div>
+        <Clicker inc={inc} />
+        <Count count={count} />
+      </div>
+    </ThingsProvider>
   );
 };
 
